9cc6f816de06215e6012e675d3fb5cbe52ae067e
Marc Nebot <newgate3211@gmail.com>
2024-12-05 12:11:02 +0100

    subido correccion profe

---
+++ frontend/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.css'; // Cambia según tu archivo de estilos
+import app from './app';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+<React.StrictMode>
+    <App />
+</React.StrictMode>
+);
