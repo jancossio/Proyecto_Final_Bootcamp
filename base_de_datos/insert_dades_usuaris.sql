e23c9ff956179a712cc6566c0b93ef65c7c38740
Marc Nebot <newgate3211@gmail.com>
2024-12-05 12:11:49 +0100

    correcion profe

---
+++ base_de_datos/insert_dades_usuaris.sql
@@ -0,0 +1,7 @@
+USE db_fitracking;
+
+INSERT INTO dades_usuari (email, contra, edat, pes, altura, activitatfisica, problemesalut, litresaigua, alergia, motivacio, objectiu, preferencialimentaria)
+VALUES ('maria.gomez@example.com', '67890', 25, 65.2, 1.68, 'Alta', 'Asma', 2.5, 'Ninguna', 'Ganar masa muscular', 'Aumentar fuerza', 'Omnívora');
+
+INSERT INTO dades_usuari (email, contra, edat, pes, altura, activitatfisica, problemesalut, litresaigua, alergia, motivacio, objectiu, preferencialimentaria)
+VALUES ('pedro.lopez@example.com', 'abc123', 40, 85.3, 1.80, 'Baja', 'Hipertensión', 1.8, 'Ninguna', 'Mantener peso', 'Mejorar salud cardiovascular', 'Vegana');
