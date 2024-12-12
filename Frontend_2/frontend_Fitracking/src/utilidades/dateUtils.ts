cae9689ff11a448987513a40cbee8afa017613e3
unknown <polvr17@gmail.com>
2024-12-12 13:33:29 +0100

    seguimiento peso todo perfecto

--- Frontend_2/frontend_Fitracking/src/utilidades/dateUtils.ts
+++ Frontend_2/frontend_Fitracking/src/utilidades/dateUtils.ts
@@ -1,7 +1,7 @@
 import { addMonths, startOfMonth } from 'date-fns';
 
 export function obtenerFechaInicial(): Date {
-  return new Date(2024, 11, 1); // 1 de diciembre de 2023
+  return new Date(2024, 11, 1); 
 }
 
 export function generarFechaMensual(fechaAnterior: Date): Date {
